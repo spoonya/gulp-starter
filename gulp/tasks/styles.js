758eae9019b6ff1e91c1a6050e10aa12a259d5ad
spoonya <nomad8t@gmail.com>
2022-10-08 11:58:52 +0300

    upd-2022

---
+++ gulp/tasks/styles.js
@@ -0,0 +1,50 @@
+const gulp = require('gulp');
+const plumber = require('gulp-plumber');
+const sass = require('gulp-sass');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const shorthand = require('gulp-shorthand');
+const autoprefixer = require('gulp-autoprefixer');
+const gulpStylelint = require('gulp-stylelint');
+const mediaGroup = require('gulp-group-css-media-queries');
+
+module.exports = function styles() {
+  return gulp
+    .src('src/sass/*.scss')
+    .pipe(plumber())
+    .pipe(
+      gulpStylelint({
+        failAfterError: false,
+        reporters: [
+          {
+            formatter: 'string',
+            console: true
+          }
+        ]
+      })
+    )
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(
+      autoprefixer({
+        cascade: false
+      })
+    )
+    .pipe(shorthand())
+    .pipe(
+      cleanCSS(
+        {
+          debug: true,
+          compatibility: '*'
+        },
+        (details) => {
+          console.log(
+            `${details.name}: Original size:${details.stats.originalSize} - Minified size: ${details.stats.minifiedSize}`
+          );
+        }
+      )
+    )
+    .pipe(mediaGroup())
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('build/assets/css'));
+};
